c0ff1064d496dcf6bab5d69ce88aaf1c6533322b
Бексултан <beksultan@macbook-air-beksultan.local>
2018-04-16 23:45:02 +0600

    Initial Commit

---
+++ Komek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Komek
+//
+//  Created by Бексултан on 16.04.2018.
+//  Copyright © 2018 Бексултан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
